4852f07df57b1d7ccb626e9d3fd2f95cd0467f7e
fabohlus <45484002+fabohlus@users.noreply.github.com>
2022-02-01 09:46:52 +0100

    add cartReducers, add cartConstants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,6 @@
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 //create redux store to use in react application => returns a list of products
@@ -7,6 +8,7 @@ const initialstate = {};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
