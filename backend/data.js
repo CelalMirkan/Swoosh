eb8b9918290f7e0ccae9201859af2a0e84fa5aae
CelalMirkan <mirkan.albayrak@gmail.com>
2022-02-01 20:51:31 +0100

    Added MongoDB Atlas

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,22 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+
+    users: [
+        {
+            name:'Mirkan',
+            email:'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+
+        {
+            name:'Fabian',
+            email:'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
 
     products: [
 
