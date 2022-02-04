fe3af878ead01fc755bd74a836504502153661c4
CelalMirkan <mirkan.albayrak@gmail.com>
2022-02-04 10:25:42 +0100

    Added B2B Endpoints

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import User from './models/userModel.js';
 import Order from './models/orderModel.js';
+import bcrypt from 'bcryptjs';
 
 dotenv.config();
 const app = express();
@@ -31,13 +32,68 @@ app.get('/api/b2b/customers', async(req, res) => {
     return res.send(cust);
 });
 
+//B2B endpoint to get order history given user ID
 app.get('/api/b2b/orders', async(req, res) => {
-    const customer = req.params.id;
-    console.log(req.params.id);
+    const customer = req.query.user;
+    console.log(req.query.user);
 
-    const oders = await Order.find({user:customer})
-    return res.send(oders);
-})
+    const orders = await Order.find({user:customer})
+    return res.send(orders);
+});
+
+//endpoint b2b order procurement
+app.post('/api/b2b/orders/buy', async(req, res) => {
+
+    const ordered = new Order({
+        orderitems: {
+            name: req.query.name,
+            qty: 1,
+            image: "/images/p3.jpg",
+            price: 220,
+            product: "61fa5e14127e4c5351ac29a7",
+        },
+        shippingAddress: {
+            fullName: req.query.shipname,
+            address: req.query.shipaddress,
+            city: req.query.shipcity,
+            postalCode: req.query.shipzip,
+            country: req.query.shipcountry,
+        },
+        paymentMethod : req.query.paymentMethod,
+        itemsPrice :  220,
+        shippingPrice : 0,
+        taxPrice : 46,
+        totalPrice : 266,
+        user :  req.query.id
+    });
+
+    const createdOrder = await ordered.save();
+    res.status(201).send({message: 'New Order created!', order: createdOrder});
+
+});
+
+//B2B endpoint to register user
+app.post('/api/b2b/register', async(req, res) => {
+
+    const user = new User({
+        name: req.query.name,
+        email: req.query.email,
+        password: bcrypt.hashSync(req.query.password, 8),
+    });
+
+    const createdUser = await user.save();
+    res.send({
+        _id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        isAdmin: createdUser.isAdmin,
+        //token: generateToken(createdUser),
+    });
+    
+});
+
+//endpoint to search for products, given a shoe name
+app.use('/api/b2b/products', productRouter);
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message});
